40de64b98134329cb70e742bc51f11741bbed609
Kristine Christensen <67657119+kristinechristensen@users.noreply.github.com>
2024-05-27 14:13:38 -0500

    updateFileStructure

---
+++ cybersecurta/app/page.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/logo.png"
+          alt="CyberSecurTA"
+          width={283}
+          height={228}
+          priority
+        />
+      </div>
+      <div className={styles.center}>
+      <p className={styles.title1}>Coming Summer 2024!</p>
+      </div>
+
+      
+    </main>
+  );
+}
